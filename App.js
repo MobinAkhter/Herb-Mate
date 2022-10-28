2b4d922d28cc4feaf1a78fb82906193c07be7d66
Mobin Akhter <akhtemob@sheridancollege.ca>
2022-10-27 22:41:21 -0400

    This code didn't get pushed for some reason.

--- App.js
+++ App.js
@@ -1,159 +1,59 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  TextInput,
-  Pressable,
-} from "react-native";
-import styles from "./styles";
-import Svg, { Image, Ellipse, ClipPath } from "react-native-svg";
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  interpolate,
-  withTiming,
-  withDelay,
-  runOnJS,
-  withSequence,
-  withSpring,
-} from "react-native-reanimated";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import { Colors } from "./constants/styles";
+
+// Add references here
+
+const Stack = createNativeStackNavigator();
+
+function AuthStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.darkGreen },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.darkGreen },
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
+  );
+}
+// Switched to another branch
+function AuthenticatedStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.greenColor },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.greenColor },
+      }}
+    >
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
+  );
+}
 
 export default function App() {
-  const { height, width } = Dimensions.get("window");
-  const imagePosition = useSharedValue(1);
-  const formButtonScale = useSharedValue(1);
-  const [isRegistering, setIsRegistering] = useState(false);
-
-  const imageAnimatedStyle = useAnimatedStyle(() => {
-    const interpolation = interpolate(
-      imagePosition.value,
-      [0, 1],
-      [-height / 2, 0]
-    );
-    return {
-      transform: [
-        { translateY: withTiming(interpolation, { duration: 1000 }) },
-      ],
-    };
-  });
-
-  const buttonsAnimatedStyle = useAnimatedStyle(() => {
-    const interpolation = interpolate(imagePosition.value, [0, 1], [250, 0]);
-    return {
-      opacity: withTiming(imagePosition.value, { duration: 500 }),
-      transform: [
-        { translateY: withTiming(interpolation, { duration: 1000 }) },
-      ],
-    };
-  });
-
-  const closeButtonContainerStyle = useAnimatedStyle(() => {
-    const interpolation = interpolate(imagePosition.value, [0, 1], [180, 360]);
-    return {
-      opacity: withTiming(imagePosition.value === 1 ? 0 : 1, { duration: 800 }),
-      transform: [
-        { rotate: withTiming(interpolation + "deg", { duration: 1000 }) },
-      ],
-    };
-  });
-
-  const formAnimatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity:
-        imagePosition.value === 0
-          ? withDelay(400, withTiming(1, { duration: 800 }))
-          : withTiming(0, { duration: 300 }),
-    };
-  });
-
-  const formButtonAnimatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ scale: formButtonScale.value }],
-    };
-  });
-
-  const loginHandler = () => {
-    imagePosition.value = 0;
-    if (isRegistering) {
-      runOnJS(setIsRegistering)(false);
-    }
-  };
-
-  const registerHandler = () => {
-    imagePosition.value = 0;
-    if (!isRegistering) {
-      runOnJS(setIsRegistering)(true);
-    }
-  };
-
   return (
-    <Animated.View style={styles.container}>
-      <Animated.View style={[StyleSheet.absoluteFill, imageAnimatedStyle]}>
-        <Svg height={height + 100} width={width}>
-          <ClipPath id="clipPathId">
-            <Ellipse cx={width / 2} rx={height} ry={height + 100} />
-          </ClipPath>
-          <Image
-            href={require("./assets/login-background.jpg")}
-            width={width + 100}
-            height={height + 100}
-            preserveAspectRatio="xMidYMid slice"
-            clipPath="url(#clipPathId)"
-          />
-        </Svg>
-        <Animated.View
-          style={[styles.closeButtonContainer, closeButtonContainerStyle]}
-        >
-          <Text onPress={() => (imagePosition.value = 1)}>X</Text>
-        </Animated.View>
-      </Animated.View>
-      <View style={styles.bottomContainer}>
-        <Animated.View style={buttonsAnimatedStyle}>
-          <Pressable style={styles.button} onPress={loginHandler}>
-            <Text style={styles.buttonText}>LOG IN</Text>
-          </Pressable>
-        </Animated.View>
-        <Animated.View style={buttonsAnimatedStyle}>
-          <Pressable style={styles.button} onPress={registerHandler}>
-            <Text style={styles.buttonText}>REGISTER</Text>
-          </Pressable>
-        </Animated.View>
-        <Animated.View style={[styles.formInputContainer, formAnimatedStyle]}>
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor="black"
-            style={styles.textInput}
-          />
-          {isRegistering && (
-            <TextInput
-              placeholder="Full Name"
-              placeholderTextColor="black"
-              style={styles.textInput}
-            />
-          )}
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="black"
-            style={styles.textInput}
-          />
-          <Animated.View style={[styles.formButton, formButtonAnimatedStyle]}>
-            <Pressable
-              onPress={() =>
-                (formButtonScale.value = withSequence(
-                  withSpring(1.5),
-                  withSpring(1)
-                ))
-              }
-            >
-              <Text style={styles.buttonText}>
-                {isRegistering ? "REGISTER" : "LOG IN"}
-              </Text>
-            </Pressable>
-          </Animated.View>
-        </Animated.View>
-      </View>
-    </Animated.View>
+    <>
+      <StatusBar style="light" />
+
+      <Navigation />
+    </>
   );
 }
