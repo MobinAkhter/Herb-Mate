5f8a18fa36c9143687d3b0e0f45748ccfd393313
Mobin Akhter <akhtemob@sheridancollege.ca>
2022-12-13 04:27:40 -0500

    Put a search bar on the home page.

--- components/Home/WelcomePage.js
+++ components/Home/WelcomePage.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Fontisto } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
-
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 import BigButton from "../ui/BigButton";
 import { db } from "../../firebase";
 
@@ -34,85 +34,105 @@ function WelcomePage({}) {
   }, []);
 
   return (
-    <FlatList
-      data={bodyParts}
-      renderItem={({ item }) => (
-        <View style={styles.container}>
-          <BigButton
-            onPress={() => {
-              navigation.navigate("Condition", {
-                bp: item.name,
-              });
-            }}
-          >
-            {/* Determines icon to show based on bodypart */}
-            <View style={styles.icon}>
-              {item.name === "Digestive" && (
-                <MaterialCommunityIcons
-                  styles={styles.icon}
-                  name="stomach"
-                  size={40}
-                  color="black"
-                />
-              )}
-              {item.name === "Circulatory" && (
-                <Fontisto
-                  styles={styles.icon}
-                  name="blood-drop"
-                  size={40}
-                  color="black"
-                />
-              )}
-              {item.name === "Head and Neck" && (
-                <FontAwesome5
-                  styles={styles.icon}
-                  name="head-side-virus"
-                  size={40}
-                  color="black"
-                />
-              )}
-              {item.name === "Mental" && (
-                <MaterialCommunityIcons
-                  styles={styles.icon}
-                  name="brain"
-                  size={40}
-                  color="black"
-                />
-              )}
-              {item.name === "Respiratory" && (
-                <FontAwesome5
-                  styles={styles.icon}
-                  name="lungs"
-                  size={40}
-                  color="black"
-                />
-              )}
-              {item.name === "Skeletal" && (
-                <FontAwesome5
-                  styles={styles.icon}
-                  name="bone"
-                  size={40}
-                  color="black"
-                />
-              )}
-              {item.name === "Skin" && (
-                <Ionicons
-                  styles={styles.icon}
-                  name="body"
-                  size={40}
-                  color="black"
-                />
-              )}
-            </View>
+    <View>
+      <View style={styles.search}>
+        <MagnifyingGlassIcon color="gray" size={20} />
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search by herbs or symptoms"
+          keyboardType="default"
+        />
+      </View>
+      <FlatList
+        data={bodyParts}
+        renderItem={({ item }) => (
+          <View style={styles.container}>
+            <BigButton
+              onPress={() => {
+                navigation.navigate("Condition", {
+                  bp: item.name,
+                });
+              }}
+            >
+              {/* Determines icon to show based on bodypart */}
+              <View style={styles.icon}>
+                {item.name === "Digestive" && (
+                  <MaterialCommunityIcons
+                    styles={styles.icon}
+                    name="stomach"
+                    size={40}
+                    color="black"
+                  />
+                )}
+                {item.name === "Circulatory" && (
+                  <Fontisto
+                    styles={styles.icon}
+                    name="blood-drop"
+                    size={40}
+                    color="black"
+                  />
+                )}
+                {item.name === "Head and Neck" && (
+                  <FontAwesome5
+                    styles={styles.icon}
+                    name="head-side-virus"
+                    size={40}
+                    color="black"
+                  />
+                )}
+                {item.name === "Mental" && (
+                  <MaterialCommunityIcons
+                    styles={styles.icon}
+                    name="brain"
+                    size={40}
+                    color="black"
+                  />
+                )}
+                {item.name === "Respiratory" && (
+                  <FontAwesome5
+                    styles={styles.icon}
+                    name="lungs"
+                    size={40}
+                    color="black"
+                  />
+                )}
+                {item.name === "Skeletal" && (
+                  <FontAwesome5
+                    styles={styles.icon}
+                    name="bone"
+                    size={40}
+                    color="black"
+                  />
+                )}
+                {item.name === "Skin" && (
+                  <Ionicons
+                    styles={styles.icon}
+                    name="body"
+                    size={40}
+                    color="black"
+                  />
+                )}
+              </View>
 
-            {item.name}
-          </BigButton>
-        </View>
-      )}
-    />
+              {item.name}
+            </BigButton>
+          </View>
+        )}
+      />
+    </View>
   );
 }
 
 export default WelcomePage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  search: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "lightgray",
+  },
+  searchInput: {
+    backgroundColor: "lightgray",
+  },
+});
