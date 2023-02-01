82c04cb176791c16d95ba4106171f5fe6bd0b71a
Shane Guther <guthers@sheridancollege.ca>
2023-01-31 19:53:25 -0500

    Centered content on AppSettingsScreen.js

--- screens/AppSettingsScreen.js
+++ screens/AppSettingsScreen.js
@@ -1,33 +1,34 @@
 import AppSettings from "../components/Home/AppSettings";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 const AppSettingsScreen = ({ navigation }) => {
-    return (
-      <View>
-        <AppSettings/>
-      </View>
-    );
-  };
-  
-  export default AppSettingsScreen;
+  return (
+    <View style={styles.rootContainer}>
+      <View style={styles.header}></View>
+      <AppSettings />
+    </View>
+  );
+};
 
-  const styles = StyleSheet.create({
-    header: {
-      flexDirection: "row",
-      paddingTop: 25,
-      justifyContent: "space-between",
-      alignItems: "center",
-      width: "100%",
-      padding: 16,
-      backgroundColor: "#35D96F",
-    },
-    rootContainer: {
-      flex: 1,
-      alignItems: "center",
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: "bold",
-      color: "white",
-    },
-  });
+export default AppSettingsScreen;
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    paddingTop: 25,
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    padding: 16,
+    backgroundColor: "#35D96F",
+  },
+  rootContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
