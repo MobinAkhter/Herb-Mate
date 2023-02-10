cae21dc2de090d7990c3ba85a36fa10c3c14fa87
Anais <anaiszed447@gmail.com>
2023-02-09 23:56:11 -0500

    Made updates

--- screens/AppSettingsScreen.js
+++ screens/AppSettingsScreen.js
@@ -1,7 +1,5 @@
 import AppSettings from "../components/Home/AppSettings";
-
-import { StyleSheet, View } from "react-native";
-
+import { View, StyleSheet } from "react-native";
 
 const AppSettingsScreen = ({ navigation }) => {
   return (
@@ -12,26 +10,25 @@ const AppSettingsScreen = ({ navigation }) => {
   );
 };
 
+export default AppSettingsScreen;
 
-  const styles = StyleSheet.create({
-    header: {
-      flexDirection: "row",
-      paddingTop: 25,
-      justifyContent: "space-between",
-      alignItems: "center",
-      width: "100%",
-      padding: 16,
-      backgroundColor: "#35D96F",
-    },
-    rootContainer: {
-      flex: 1,
-      alignItems: "center",
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: "bold",
-      color: "white",
-    },
-  });
-  
-
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    paddingTop: 25,
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    padding: 16,
+    backgroundColor: "#35D96F",
+  },
+  rootContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
