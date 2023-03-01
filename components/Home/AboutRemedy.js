2a896aa473b0b2aab81116dbd5461301430d64b9
Anais <anaiszed447@gmail.com>
2023-03-01 18:33:12 -0500

    Implement remedies into user bookmarks collection when they click bookmark button.

--- components/Home/AboutRemedy.js
+++ components/Home/AboutRemedy.js
@@ -1,13 +1,35 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
-import { db } from "../../firebase";
+import { StyleSheet, View, Text, ScrollView, Image, Alert } from "react-native";
+import { db, auth } from "../../firebase";
 import BigButton from "../ui/BigButton";
 import BookMarkButton from "../ui/BookmarkButton";
+import {getAuth} from 'firebase/auth'
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 function AboutRemedy({ bodyPart, condition, rem }) {
   const [remedy, setRemedy] = useState({});
+  const [bookMarkText, setBookMarkText] = useState("BookMark")
+ const [checkBookMark, setCheckBookMark] = useState("")
+ const [buttonColor, setButtonColor] = useState("red")
+
   const col = db.collection("BodyParts");
   console.log("rem: ", rem);
+
+  //access firestore
+ 
+ 
+  //accessing current authenticated user
+  
+  //accessing the current user in firestore users collection in database by finding record through user id
+  const user = auth.currentUser.uid
+
+  const userRef = db.collection('users').doc(user);
+
+  //accessing the bookmarks collection from the user
+  //const bookmarks =  userRef.collection('bookmarks').doc('remedy').get();
+
 
   //Gets specific herbal remedy to show information about
   //currently has an issue getting the document id.  rem parameter should give
@@ -29,6 +51,81 @@ function AboutRemedy({ bodyPart, condition, rem }) {
       });
   }, []);
 
+  function bookMarkRemedy()
+  {
+
+   
+    userRef.get().then((doc) => {
+      if (doc.exists) {
+        const data = doc.data();
+        const email = data.email;
+        const firstName = data.firstName;
+        const lastName = data.lastName;
+        console.log('Email:', email);
+        console.log('First name:', firstName);
+        console.log('Last name:', lastName);
+
+        //console.log(doc.data().collection("bookmarks"))
+
+        
+      } else {
+        console.log('No such document!');
+      }
+    }).catch((error) => {
+      console.log('Error getting document:', error);
+    });
+
+
+
+    if(checkBookMark == false){
+        userRef.collection('bookmarks')
+          .doc(rem)
+          .set({
+            name: remedy.name,
+            description: remedy.description,
+            image : remedy.image,
+            precautions: remedy.precautions
+          })
+      
+          Alert.alert(
+            `${remedy.name} has been bookmarked!`
+           )
+          setBookMarkText("UNBOOKMARK")
+         
+          setCheckBookMark(true)
+    }else{
+      userRef.collection('bookmarks').doc(rem).delete()
+      
+      Alert.alert(
+        `${remedy.name} has been removed from your bookmarks!`
+       )
+      setBookMarkText("BOOKMARK")
+      setCheckBookMark(false)
+    }
+
+   
+
+  }
+
+  
+  useEffect(() => {
+   const checkRemedy = userRef.collection('bookmarks').doc(rem)
+      checkRemedy.get()
+          .then((docSnapshot) => {
+            if (docSnapshot.exists){
+              console.log("true")
+              setBookMarkText("UNBOOKMARK")
+              setCheckBookMark(true)
+              
+            }else{
+              console.log('false')
+              setBookMarkText("BOOKMARK")
+              setCheckBookMark(false)
+            }
+          });
+        
+  })
+
   return (
     <ScrollView>
       <View style={styles.container}>
@@ -48,7 +145,11 @@ function AboutRemedy({ bodyPart, condition, rem }) {
           <Text style={styles.head}>Precautions</Text>
           <Text style={styles.desc}>{remedy.precautions}</Text>
         </View>
-        <BookMarkButton>BOOKMARK</BookMarkButton>
+        <BookMarkButton 
+        onPress={bookMarkRemedy}
+        style={{ backgroundColor: "red" }} >
+          {bookMarkText}
+          </BookMarkButton>
       </View>
     </ScrollView>
   );
