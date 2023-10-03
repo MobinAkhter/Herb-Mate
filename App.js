aaa0ac590ac8ae86de4f041566bcdb880c1cdd56
Mobin Akhter <akhtemob@sheridancollege.ca>
2023-10-02 22:10:58 -0400

    here goes nothing

--- App.js
+++ App.js
@@ -27,6 +27,7 @@ function Navigation() {
   // Updating EXPO SDK SO that the app can run on physcial device using Expo Go.
   // Will try to fix these buttons and make them dynamic, so that the text shows up nicely.
   //Apparently last time, it automatically wanted me to upgrade to 48.0.0, i'll try 47.0.0, if it does not work, then I quit.
+  // Trying once again to update the sdk. Won't quit before I resolve this. Will need to be able to say in the defense it has been tested by other people, we published it on google play for extra points.
   return (
     <NavigationContainer>
       {user ? <AuthenticatedStack /> : <AuthStack />}
