d74233a83af74a47180f2e524e72fb10317ebcc2
Mobin Akhter <akhtemob@sheridancollege.ca>
2023-10-07 23:32:18 -0400

    made styling changes to forgot pw screen. Added pn phrases

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const {Expo} = require("expo-server-sdk");
-const {SessionsClient} = require("@google-cloud/dialogflow");
+const { Expo } = require("expo-server-sdk");
+const { SessionsClient } = require("@google-cloud/dialogflow");
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -25,6 +25,56 @@ const notificationMessages = [
   "Updates in our remedies list! Discover what's new on HerbalLife.",
   "From Basil to Turmeric, unveil nature's secrets on HerbalLife.",
   "Tip: Turmeric is anti-inflammatory. Better with black pepper!",
+  "Aconite: A powerful herb for pain relief. Explore more on HerbalLife!",
+  "Discover the calming properties of Agrimony on HerbalLife!",
+  "Unlock the secrets of Aloe for skin health. Dive in now!",
+  "Angelica: Nature's remedy for digestion. Learn more on HerbalLife!",
+  "Boost heart health with Arjun. Explore on HerbalLife!",
+  "Arnica: Your natural solution for bruises and pain. Discover more!",
+  "Ashwagandha: Stress relief from nature. Visit HerbalLife!",
+  "Astragalus: Enhance your immune system naturally. Learn how!",
+  "Barberry: A herbal remedy for digestive health. Explore on HerbalLife!",
+  "Basil: Not just for cooking – discover its health benefits!",
+  "Bay laurel: Aromatic herb with wellness benefits. Dive in now!",
+  "Bilberry: Good for eyes and more. Explore on HerbalLife!",
+  "Black Cohosh: Natural relief for menopause. Learn more!",
+  "Boswellia: Tackle inflammation the natural way. Visit HerbalLife!",
+  "Buchu: Herbal remedy for urinary health. Discover more!",
+  "Bupleurum: Balance your body with this ancient herb.",
+  "Burdock: Detoxify and revitalize with this herbal remedy.",
+  "Butcher’s Broom: Improve circulation naturally. Learn how!",
+  "Butterbur: Say goodbye to migraines. Explore on HerbalLife!",
+  "Calendula: Gentle herb for skin and wellness. Dive in now!",
+  "California Poppy: Natural aid for relaxation. Visit HerbalLife!",
+  "Cayenne: Spice up your health with this herb. Learn more!",
+  "Chickweed: Discover the healing power of this herb.",
+  "Chrysanthemum: A soothing herb for stress relief.",
+  "Comfrey: Nature's remedy for joint and muscle health.",
+  "Cornflower: Herbal beauty secrets. Explore on HerbalLife!",
+  "Cramp Bark: Alleviate menstrual cramps naturally.",
+  "Cranberry: Support urinary tract health. Dive in now!",
+  "Dandelion: Detoxify your body with this herbal hero.",
+  "Deadly Nightshade: Beware, but learn about its history.",
+  "Dong Quai: Balance hormones naturally. Visit HerbalLife!",
+  "Echinacea: Boost your immune system with this herb.",
+  "Elder: Traditional remedy for colds and flu. Learn more!",
+  "Elecampane: Respiratory wellness from nature. Explore now!",
+  "Eucalyptus: Breathe easier with this herbal remedy.",
+  "Evening Primrose: Enhance skin and hormonal health.",
+  "Eyebright: Herbal support for eye health. Dive in now!",
+  "Feverfew: Your migraine solution from nature. Visit HerbalLife!",
+  "Flaxseed: Superfood for heart and digestion. Learn more!",
+  "Fo Ti: Ancient remedy for longevity and vitality.",
+  "Frankincense: Explore the history and benefits of this resin.",
+  "Garlic: The herbal powerhouse for immune support.",
+  "Gentian: Herbal remedy for digestion and appetite.",
+  "German Chamomile: Calm your nerves the natural way.",
+  "Ginger: Spice up your health with this herbal favorite.",
+  "Ginkgo: Enhance memory and circulation. Dive in now!",
+  "Ginseng: Vitality and energy from nature. Visit HerbalLife!",
+  "Goji Berry: Discover the antioxidant benefits of this superfood.",
+  "Goldenseal: Herbal remedy for immune and digestive health.",
+  "Gotu Kola: Brain health and more with this herb.",
 ];
 
 exports.dialogflowGateway = functions.https.onRequest(async (req, res) => {
@@ -34,7 +84,7 @@ exports.dialogflowGateway = functions.https.onRequest(async (req, res) => {
     return res.status(405).send("Method Not Allowed");
   }
 
-  const {text} = req.body;
+  const { text } = req.body;
 
   const sessionPath = client.projectAgentSessionPath(projectId, sessionId);
 
@@ -52,7 +102,7 @@ exports.dialogflowGateway = functions.https.onRequest(async (req, res) => {
     const responses = await client.detectIntent(request);
     const result = responses[0].queryResult.fulfillmentText;
     console.log("Received response from Dialogflow:", result);
-    return res.status(200).send({message: result});
+    return res.status(200).send({ message: result });
   } catch (error) {
     console.error("Error calling Dialogflow:", error.message);
     return res.status(500).send(error);
@@ -61,46 +111,46 @@ exports.dialogflowGateway = functions.https.onRequest(async (req, res) => {
 
 // This function pushes notifications, will need to see how it works.
 exports.sendPushNotifications = functions.pubsub
-    .schedule("15 21 * * *")
-    .timeZone("America/Toronto")
-    .onRun(async (context) => {
-      const messages = [];
-      const usersSnapshot = await db.collection("users").get();
+  .schedule("15 21 * * *")
+  .timeZone("America/Toronto")
+  .onRun(async (context) => {
+    const messages = [];
+    const usersSnapshot = await db.collection("users").get();
 
-      usersSnapshot.forEach((doc) => {
-        const expoPushToken = doc.data().expoPushToken;
+    usersSnapshot.forEach((doc) => {
+      const expoPushToken = doc.data().expoPushToken;
 
-        const randomIndex = Math.floor(
-            Math.random() * notificationMessages.length,
-        );
-        const randomMessage = notificationMessages[randomIndex];
-        if (Expo.isExpoPushToken(expoPushToken)) {
-          messages.push({
-            to: expoPushToken,
-            sound: "default",
-            body: randomMessage,
-            data: {withSome: "data"},
-          });
-        }
-      });
-
-      console.log("Preparing to send push notifications...");
-
-      const chunks = expo.chunkPushNotifications(messages);
-      const tickets = [];
-      for (const chunk of chunks) {
-        try {
-          const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-          console.log(
-              "Successfully sent a chunk of push notifications:",
-              ticketChunk,
-          );
-          tickets.push(...ticketChunk);
-        } catch (error) {
-          console.error(
-              "Error sending a chunk of push notifications:",
-              error.message,
-          );
-        }
+      const randomIndex = Math.floor(
+        Math.random() * notificationMessages.length
+      );
+      const randomMessage = notificationMessages[randomIndex];
+      if (Expo.isExpoPushToken(expoPushToken)) {
+        messages.push({
+          to: expoPushToken,
+          sound: "default",
+          body: randomMessage,
+          data: { withSome: "data" },
+        });
       }
     });
+
+    console.log("Preparing to send push notifications...");
+
+    const chunks = expo.chunkPushNotifications(messages);
+    const tickets = [];
+    for (const chunk of chunks) {
+      try {
+        const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        console.log(
+          "Successfully sent a chunk of push notifications:",
+          ticketChunk
+        );
+        tickets.push(...ticketChunk);
+      } catch (error) {
+        console.error(
+          "Error sending a chunk of push notifications:",
+          error.message
+        );
+      }
+    }
+  });
