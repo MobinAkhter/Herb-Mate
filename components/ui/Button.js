7e5f1dd6f168e6b4ca747a9a0c338c7dcc46799e
Mobin Akhter <akhtemob@sheridancollege.ca>
2023-11-19 17:02:23 -0500

    app seems to function properly without the use of three ui files code.

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,39 +1,39 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+// import { Pressable, StyleSheet, Text, View } from "react-native";
 
-function Button({ children, onPress }) {
-  return (
-    <Pressable
-      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
-      onPress={onPress}
-    >
-      <View>
-        <Text style={styles.buttonText}>{children}</Text>
-      </View>
-    </Pressable>
-  );
-}
+// function Button({ children, onPress }) {
+//   return (
+//     <Pressable
+//       style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+//       onPress={onPress}
+//     >
+//       <View>
+//         <Text style={styles.buttonText}>{children}</Text>
+//       </View>
+//     </Pressable>
+//   );
+// }
 
-export default Button;
+// export default Button;
 
-const styles = StyleSheet.create({
-  button: {
-    borderRadius: 6,
-    paddingVertical: 6,
-    paddingHorizontal: 12,
-    backgroundColor: "white",
-    elevation: 2,
-    shadowColor: "black",
-    shadowOffset: { width: 1, height: 1 },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-  },
-  pressed: {
-    opacity: 0.7,
-  },
-  buttonText: {
-    textAlign: "center",
-    color: "black",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
+// const styles = StyleSheet.create({
+//   button: {
+//     borderRadius: 6,
+//     paddingVertical: 6,
+//     paddingHorizontal: 12,
+//     backgroundColor: "white",
+//     elevation: 2,
+//     shadowColor: "black",
+//     shadowOffset: { width: 1, height: 1 },
+//     shadowOpacity: 0.25,
+//     shadowRadius: 4,
+//   },
+//   pressed: {
+//     opacity: 0.7,
+//   },
+//   buttonText: {
+//     textAlign: "center",
+//     color: "black",
+//     fontSize: 16,
+//     fontWeight: "bold",
+//   },
+// });
