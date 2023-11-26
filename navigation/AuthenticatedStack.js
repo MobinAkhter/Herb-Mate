5d6342efbed2fe4a5b4a3e411c12975015d02876
Shane Guther <guthers@sheridancollege.ca>
2023-11-26 17:42:27 -0500

    Fixed header back in preparation details

--- navigation/AuthenticatedStack.js
+++ navigation/AuthenticatedStack.js
@@ -232,13 +232,13 @@ export function PreparationStack() {
       <Stack.Screen
         name="Preparation Details"
         component={PreparationDetails}
-        options={({ navigation }) => ({
-          headerLeft: () => (
-            <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
-              <Ionicons name="menu-outline" size={28} />
-            </TouchableOpacity>
-          ),
-        })}
+        // options={({ navigation }) => ({
+        //   headerLeft: () => (
+        //     <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+        //       <Ionicons name="menu-outline" size={28} />
+        //     </TouchableOpacity>
+        //   ),
+        // })}
       />
     </Stack.Navigator>
   );
