0e4fb00020b6a39c456e711c5d9135978a4de625
Mobin Akhter <akhtemob@sheridancollege.ca>
2024-02-25 21:41:28 -0500

    tryna implement better image loading speed next

--- screens/Auth/SplashScreen.js
+++ screens/Auth/SplashScreen.js
@@ -1,8 +1,25 @@
 import React, { useEffect } from "react";
-import { View, Text, StyleSheet, Image, StatusBar } from "react-native";
+import { View, StyleSheet, Animated, Easing } from "react-native";
 
 const SplashScreen = ({ navigation }) => {
+  const fadeAnim = new Animated.Value(0);
+  const scaleAnim = new Animated.Value(0);
+
   useEffect(() => {
+    Animated.parallel([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(scaleAnim, {
+        toValue: 1,
+        duration: 2000,
+        easing: Easing.elastic(1),
+        useNativeDriver: true,
+      }),
+    ]).start();
+
     const timer = setTimeout(() => {
       navigation.replace("Signup");
     }, 3000);
@@ -12,14 +29,23 @@ const SplashScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-      <Image
-        // source={require("./path-to-logo.png")}
+      <Animated.Image
+        source={require("../../assets/leaf_icon.jpeg")}
         resizeMode="contain"
-        style={styles.logo}
+        style={[
+          styles.logo,
+          {
+            opacity: fadeAnim,
+            transform: [{ scale: scaleAnim }],
+          },
+        ]}
       />
-      <Text style={styles.title}>Welcome to Herb Mate</Text>
-      <Text style={styles.subtitle}>Discover the world of herbs</Text>
+      <Animated.Text style={[styles.title, { opacity: fadeAnim }]}>
+        Welcome to Herb Mate
+      </Animated.Text>
+      <Animated.Text style={[styles.subtitle, { opacity: fadeAnim }]}>
+        Discover the world of herbs
+      </Animated.Text>
     </View>
   );
 };
@@ -29,22 +55,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f7f7f7", // A light grey background color
+    backgroundColor: "#f7f7f7",
   },
   logo: {
-    width: 150, // Adjust according to your logo's aspect ratio
+    width: 150,
     height: 150,
     marginBottom: 30,
   },
   title: {
     fontSize: 28,
-    fontWeight: "700", // Bold font weight
-    color: "#34A853", // Primary color for your brand
+    fontWeight: "700",
+    color: "#34A853",
     marginBottom: 10,
   },
   subtitle: {
     fontSize: 18,
-    color: "#555", // Subdued text color for contrast
+    color: "#555",
     marginTop: 5,
   },
 });
